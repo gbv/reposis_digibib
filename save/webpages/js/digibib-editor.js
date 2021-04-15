f5f52cb0fde39f86cf67f445c3ca9d025895160b
Lennard Golsch <lennard.golsch@icloud.com>
2021-04-15 09:35:11 +0200

    fixed init

--- save/webpages/js/digibib-editor.js
+++ save/webpages/js/digibib-editor.js
@@ -1,27 +1,44 @@
 $(document).ready(function() {
-    $("body").on("change", ".publication-type-select", function () {
-        const type = $(this).val();
-        let parent = $(this).parent().parent().parent().parent();
-        let container = $(".embargo-container");
-        if (type == "oae") {
+    function handlePublicationTypeSelect(value) {
+        const parent = $(".mir-publication");
+        const container = $(".mir-publication-embargo-container");
+        if (value == "oae") {
             container.removeClass("d-none");
-            parent.addClass("mir-fieldset-content")
+            parent.addClass("mir-fieldset-content");
         } else {
             container.addClass("d-none");
-            parent.removeClass("mir-fieldset-content")
+            parent.removeClass("mir-fieldset-content");
         }
-    });
+    }
 
-    $("body").on("change", ".project-type-select", function () {
-        const type = $(this).val();
-        let container = $(".mir-openAIRE-container");
-        $(this).parent().parent().parent().parent().toggleClass("mir-fieldset-content");
-        if (type == "oaire") {
+    function handleProjectTypeSelect(value) {
+        const parent = $(".mir-project");
+        const container = $(".mir-project-openAIRE-container");
+        if (value == "oaire") {
             container.removeClass("d-none");
+            parent.addClass("mir-fieldset-content");
         } else {
             container.addClass("d-none");
+            parent.removeClass("mir-fieldset-content");
         }
+    }
+
+    function init() {
+        const publicationType = $(".mir-publication-type-select").val();
+        handlePublicationTypeSelect(publicationType);
+        const projectType = $(".mir-project-type-select").val();
+        handleProjectTypeSelect(projectType);
+    }
+
+    $("body").on("change", ".mir-publication-type-select", function () {
+        const value = $(this).val();
+        handlePublicationTypeSelect(value);
     });
 
-    //TODO on load
+    $("body").on("change", ".mir-project-type-select", function () {
+        const value = $(this).val();
+        handleProjectTypeSelect(value);
+    });
+
+    init();
 });
