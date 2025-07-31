980feed9f3b4df0e5de54e55d0e7e0c4e4c020c2
Lennard Golsch <lennard.golsch@icloud.com>
2025-07-31 12:25:13 +0200

    improvement

---
+++ src/main/java/de/vzg/reposis/digibib/agreement/AgreementObjectEventHandler.java
@@ -0,0 +1,86 @@
+package de.vzg.reposis.digibib.agreement;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.mycore.common.config.MCRConfiguration2;
+import org.mycore.common.events.MCREvent;
+import org.mycore.common.events.MCREventHandlerBase;
+import org.mycore.datamodel.metadata.MCRObject;
+import org.mycore.mods.MCRMODSWrapper;
+import org.mycore.services.queuedjob.MCRJobQueueManager;
+
+public class AgreementObjectEventHandler extends MCREventHandlerBase {
+
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    private static final String AGREEMENT_DEFAULT_NAME_CONF = "Default";
+
+    private static final String AGREEMENT_FLAG = "agreement";
+
+    private static final String STATE_PUBLISHED = "published";
+
+    private static final String CONFIG_PREFIX = "Digibib.Agreement.";
+
+    @Override
+    protected void handleObjectUpdated(MCREvent evt, MCRObject obj) {
+        final String objId = obj.getId().toString();
+        if (!isPublished(obj)) {
+            LOGGER.debug("{} is not published. Skipping...", objId);
+            return;
+        }
+        final List<String> agreements = getExistingAgreements(obj);
+        if (agreements.isEmpty()) {
+            LOGGER.debug("{} has no agreements. Skipping...", objId);
+            return;
+        }
+        final Optional<String> genre = extractGenreFromMods(obj);
+        if (genre.isEmpty()) {
+            LOGGER.debug("{} has no genre. Skipping...", objId);
+            return;
+        }
+        final Optional<String> agreementName = getAgreementName(genre.get()).or(() -> getDefaultAgreementName());
+        if (agreementName.isEmpty()) {
+            LOGGER.debug("No agreement name configured (including fallback). Skipping...");
+            return;
+        }
+        enqueueAgreementJobIfMissing(obj, agreements, agreementName.get());
+    }
+
+    private void enqueueAgreementJobIfMissing(MCRObject obj, List<String> existingAgreements,
+        String requiredAgreementName) {
+        final String objId = obj.getId().toString();
+        if (existingAgreements.contains(requiredAgreementName)) {
+            LOGGER.debug("{} already has the agreement '{}'. Skipping...", objId, requiredAgreementName);
+            return;
+        }
+        LOGGER.debug("Adding DeliverAgreementJob for {} with agreement '{}'.", objId, requiredAgreementName);
+        MCRJobQueueManager.getInstance().getJobQueue(DeliverAgreementJobAction.class)
+            .add(DeliverAgreementJobAction.createJob(obj.getId()));
+    }
+
+    private List<String> getExistingAgreements(MCRObject obj) {
+        return obj.getService().getFlags(AGREEMENT_FLAG);
+    }
+
+    private Optional<String> extractGenreFromMods(MCRObject object) {
+        return new MCRMODSWrapper(object).getElements("mods:genre").stream().map(g -> g.getAttributeValue("valueURI"))
+            .filter(Objects::nonNull).map(uri -> uri.split("#", 2)).filter(uri -> uri.length > 1).map(uri -> uri[1])
+            .findFirst();
+    }
+
+    private static Optional<String> getDefaultAgreementName() {
+        return getAgreementName(AGREEMENT_DEFAULT_NAME_CONF);
+    }
+
+    private static Optional<String> getAgreementName(String genre) {
+        return MCRConfiguration2.getString(CONFIG_PREFIX + genre + ".Name");
+    }
+
+    private boolean isPublished(MCRObject object) {
+        return Objects.equals(STATE_PUBLISHED, object.getService().getState().getId());
+    }
+}
